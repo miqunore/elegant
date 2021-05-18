7c01786243a11c3af8eccb4a260291a30cf86e3d
miqunore <82279998+miqunore@users.noreply.github.com>
2021-05-18 10:21:25 +0300

    add owl.carousel

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            1000: {
+                items: 1
+            }
+        }
+    });
+});
